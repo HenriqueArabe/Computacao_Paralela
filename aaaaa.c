589138de9945a641adef39d48fece6368e9afb64
Henrique Árabe Neres de Farias <118843050+HenriqueArabe@users.noreply.github.com>
2024-08-30 14:02:37 -0300

    Update and rename multM.c to aaaaa.c

---
+++ aaaaa.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  // Biblioteca padrão de entrada e saída
+
+int main() {
+    printf("Hello, World!\n");  // Imprime "Hello, World!" seguido de uma nova linha
+    return 0;  // Indica que o programa terminou com sucesso
+}
