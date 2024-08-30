2f3ce76cddbfb8673af47c8623b7171959fde895
Henrique Árabe Neres de Farias <118843050+HenriqueArabe@users.noreply.github.com>
2024-08-30 13:34:57 -0300

    Update mm.c

--- mm.c
+++ mm.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int TAM = 2000
+int TAM = 2000;
 
 int A[TAM][TAM];
 int B[TAM][TAM];
@@ -19,7 +19,7 @@ int main(){
 
 
   //Multiplicação
-  for(i=0;j<TAM;i++){
+  for(i=0;i<TAM;i++){
       for(k=0;k<TAM;k++){
           for(j=0;j<TAM;j++){
               C[i][j] += A[i][k] * B[k][j];
@@ -30,4 +30,6 @@ int main(){
 //Impressão
 
   fprintf(stdout, "%d\n", C[TAM-1][TAM-1]);
+
+  return 0;
 }
