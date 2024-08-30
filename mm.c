167fa076c9835ca061659f22b52bcc36ca16984c
Henrique Árabe Neres de Farias <118843050+HenriqueArabe@users.noreply.github.com>
2024-08-30 18:36:54 +0000

    Update mm.c

--- mm.c
+++ mm.c
@@ -17,8 +17,8 @@ int main() {
 
   // Multiplicação
   for (i = 0; i < 2000; i++) {
-      for (k = 0; k < 2000; k++) {
-          for (j = 0; j < 2000; j++) {
+      for (j = 0; j < 2000; j++) {
+          for (k = 0; k < 2000; k++) {
               C[i][j] += A[i][k] * B[k][j];
           }
       }
