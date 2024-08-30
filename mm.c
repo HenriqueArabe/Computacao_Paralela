8b8713f6393ff57285168e907528bc77d1059924
Henrique Árabe Neres de Farias <118843050+HenriqueArabe@users.noreply.github.com>
2024-08-30 14:05:25 -0300

    Update and rename aaaaa.c to mm.c

---
+++ mm.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int A[2000][2000];
+int B[2000][2000];
+int C[2000][2000] = {0};
+
+int main() {
+  int i, j, k;
+
+  // Inicialização
+  for (i = 0; i < 2000; i++) {
+      for (j = 0; j < 2000; j++) {
+        A[i][j] = B[i][j] = 1;
+      }
+  }
+
+  // Multiplicação
+  for (i = 0; i < 2000; i++) {
+      for (j = 0; j < 2000; j++) {
+          for (k = 0; k < 2000; k++) {
+              C[i][j] += A[i][k] * B[k][j];
+          }
+      }
+  }
+
+  // Impressão
+  fprintf(stdout, "%d\n", C[2000-1][2000-1]);
+
+  return 0; // Adicionado retorno da função main
+}
