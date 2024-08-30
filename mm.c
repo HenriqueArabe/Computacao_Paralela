d9139547c02b6436468a3bc856c66c4d04aed238
Henrique Árabe Neres de Farias <118843050+HenriqueArabe@users.noreply.github.com>
2024-08-30 10:43:04 -0300

    Create mm.c

---
+++ mm.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int TAM = 2000
+
+int A[TAM][TAM];
+int B[TAM][TAM];
+int C[TAM][TAM] ={0};
+
+int main(){
+  int i, j ,k;
+
+  //Inicialização
+  for(i=0;j<TAM;i++){
+      for(k=0;k<TAM;k++){
+        A[i][j] = B[i][j] = 1;
+      }
+  }
+
+
+  //Multiplicação
+  for(i=0;j<TAM;i++){
+      for(k=0;k<TAM;k++){
+          for(j=0;j<TAM;j++){
+              C[i][j] += A[i][k] * B[k][j];
+          }
+      }
+  }
+
+//Impressão
+
+  fprintf(stdout, "%d\n", C[TAM-1][TAM-1]);
+}
