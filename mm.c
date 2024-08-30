dc2ccf3840ca52f0f82150e353343107115b1c91
Henrique Árabe Neres de Farias <118843050+HenriqueArabe@users.noreply.github.com>
2024-08-30 13:43:06 -0300

    Update mm.c

--- mm.c
+++ mm.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int TAM = 2000;
+#define TAM 2000  // Define TAM como uma constante de tempo de compilação
 
 int A[TAM][TAM];
 int B[TAM][TAM];
-int C[TAM][TAM] ={0};
+int C[TAM][TAM] = {0};
 
-int main(){
-  int i, j ,k;
+int main() {
+  int i, j, k;
 
-  //Inicialização
-  for(i=0;j<TAM;i++){
-      for(k=0;k<TAM;k++){
+  // Inicialização
+  for (i = 0; i < TAM; i++) {
+      for (j = 0; j < TAM; j++) {
         A[i][j] = B[i][j] = 1;
       }
   }
 
-
-  //Multiplicação
-  for(i=0;i<TAM;i++){
-      for(k=0;k<TAM;k++){
-          for(j=0;j<TAM;j++){
+  // Multiplicação
+  for (i = 0; i < TAM; i++) {
+      for (k = 0; k < TAM; k++) {
+          for (j = 0; j < TAM; j++) {
               C[i][j] += A[i][k] * B[k][j];
           }
       }
   }
 
-//Impressão
-
+  // Impressão
   fprintf(stdout, "%d\n", C[TAM-1][TAM-1]);
 
   return 0;
